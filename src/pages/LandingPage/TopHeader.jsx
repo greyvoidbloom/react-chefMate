7531d8d49352bdc35337b0bfe77c43bb42d4b1ac
greyvoidbloom <kanishkapersonalprojects@gmail.com>
2024-04-19 10:50:10 +0530

    v3:lord save me

--- src/pages/LandingPage/TopHeader.jsx
+++ src/pages/LandingPage/TopHeader.jsx
@@ -8,7 +8,7 @@ const TopHeader = () => {
         <>
         <div className="top-part">
             <div className="website-logo">
-            <img src={websiteLogo} alt={"testimg"} height={"150x"}/>
+            <img src={websiteLogo} alt={"testimg"} height={"150x"} className='chefMate-logo'/>
             </div>
             <div className="top-heading">Explore Recipes</div>
             <div className="navigations">
